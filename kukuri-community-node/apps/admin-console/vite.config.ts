29316ec66b29a9ca2b6e808b833b1d42ff950fc8
KingYoSun <nocking257838@gmail.com>
2026-03-07 12:09:39 +0900

    gitignoreにcrt追加とadmin-consoleのvite.config.tsにAllowedHosts設定を追加

--- kukuri-community-node/apps/admin-console/vite.config.ts
+++ kukuri-community-node/apps/admin-console/vite.config.ts
@@ -1,22 +1,27 @@
-﻿import { defineConfig } from 'vite';
+﻿import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/admin/',
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 5173
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test/setup.ts',
-    css: true,
-    include: ['src/**/*.test.{ts,tsx}']
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: '/admin/',
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      allowedHosts: [...env.VITE_ALLOWED_HOSTS.split(",")]
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: 5173
+    },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './src/test/setup.ts',
+      css: true,
+      include: ['src/**/*.test.{ts,tsx}']
+    }
   }
 });
